79143f2ee42296feb97c171574039e4c9e23be6a
painekillers <blablablapandas@gmail.com>
2026-06-10 01:07:22 -0700

    Update e.js

--- e.js
+++ e.js
@@ -1,9 +1,12 @@
 //Yea because why not
 
-var msgFunc = G.Message;
+/*var msgFunc = G.Message;
 
 G.Message = function(obj) {
   obj.text = obj.text.replace(/\p{L}/gu, "e");
   
   msgFunc(obj);
 }
+*/
+
+console.log("E")
